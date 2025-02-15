5d5eb1ecae4995996311691c48ca866346ecf872
partha_roy <Partharoyy88@gmail.com>
2025-02-15 17:48:38 +0530

    Add zod validation

--- app/invoices/generate/index.tsx
+++ app/invoices/generate/index.tsx
@@ -4,10 +4,21 @@ import { Button } from '~/components/Button';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import CustomTextInput from '~/components/CustomTextInput';
 import { useForm, FormProvider } from 'react-hook-form';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+const senderInfoSchema = z.object({
+  name: z.string({required_error: 'Name is required'}).min(1, 'Name is required'),
+  address: z.string({required_error: 'Address is required'}).min(1, 'Address is required'),
+  taxId: z.string().optional(), 
+})
+
+type senderInfo = z.infer<typeof senderInfoSchema>
 
 const GenerateInvoice = () => {
-
-  const form = useForm()
+  const form = useForm<senderInfo>({
+    resolver: zodResolver(senderInfoSchema),
+  })
   const {handleSubmit, formState: {errors}} = form
 
   const onSubmit = (data: any) => {
@@ -17,16 +28,17 @@ const GenerateInvoice = () => {
   return (
     <KeyboardAwareScrollView className="flex-1 bg-gray-100 p-4">
       <FormProvider {...form}>
-        <Text className="text-2xl mb-5 font-bold">
-            Sender Info
-        </Text>
-        <View className='gap-2'>
-          <CustomTextInput name='name'label='Name'/>
-          <CustomTextInput name='address'label='Address' multiline />
-          <CustomTextInput name='taxId' label='Tax ID' />
+        <View className='flex-1 justify-between'>
+          <Text className="text-2xl mb-5 font-bold">
+              Sender Info
+          </Text>
+          <View className='gap-2'>
+            <CustomTextInput name='name'label='Name'/>
+            <CustomTextInput name='address'label='Address' multiline />
+            <CustomTextInput name='taxId' label='Tax ID' />
+          </View>
+          <Button title='Next' className='mt-auto' onPress={form.handleSubmit(onSubmit)}/>
         </View>
-
-        <Button title='Next' className='mt-auto' onPress={form.handleSubmit(onSubmit)}/>
       </FormProvider>
     </KeyboardAwareScrollView>
   );
